23b437d94e28fe53d0203d2d50a2f230279d89ef
Yao Jean-Claude <jeandev84@yahoo.com>
2021-01-09 18:34:53 +0300

    add and modifie address

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Address;
+use App\Form\AddressType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +17,22 @@ use Symfony\Component\Routing\Annotation\Route;
 */
 class AccountAddressController extends AbstractController
 {
+
+    /**
+     * @var EntityManagerInterface
+    */
+    private $em;
+
+
+    /**
+     * RegisterController constructor.
+     * @param EntityManagerInterface $em
+    */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
 
     /**
      * @Route("/compte/adresses", name="account_address")
@@ -30,10 +50,67 @@ class AccountAddressController extends AbstractController
 
     /**
      * @Route("/compte/ajouter-une-adresse", name="account_address_add")
-     */
-    public function add(): Response
+     * @param Request $request
+     * @return Response
+    */
+    public function add(Request $request): Response
     {
-        return $this->render('account/address_add.html.twig');
+        $address = new Address();
+
+        $form = $this->createForm(AddressType::class, $address);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $address->setUser($this->getUser());
+            /* dd($address); */
+
+            $this->em->persist($address);
+            $this->em->flush();
+
+            return $this->redirectToRoute('account_address');
+        }
+
+        return $this->render('account/address_form.html.twig', [
+            'address_form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route("/compte/modifier-une-adresse/{id}", name="account_address_edit")
+     * @param Request $request
+     * @return Response
+    */
+    public function edit(Request $request, $id): Response
+    {
+        $address = $this->em->getRepository(Address::class)
+                            ->findOneById($id);
+
+        // On verifie si l'adresse existe
+        // oubien si address n'appartient pas a mon utilisateur
+        // cad si l'utisateur de cette addresse est bel et bien l'utilisateur connecte
+        if(! $address || $address->getUser() != $this->getUser())
+        {
+            return $this->redirectToRoute('account_address');
+        }
+
+        // On verifie si l'adresse
+        $form = $this->createForm(AddressType::class, $address);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->flush();
+
+            return $this->redirectToRoute('account_address');
+        }
+
+        return $this->render('account/address_form.html.twig', [
+            'address_form' => $form->createView()
+        ]);
     }
 
 }
